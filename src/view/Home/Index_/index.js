3c0f854b8cfe0f2a88bee0fa41f4d5de6f9efcbe
haixiang <373971510@qq.com>
2020-06-07 13:27:39 +0800

    menu

--- src/view/Home/Index_/index.js
+++ src/view/Home/Index_/index.js
@@ -2,12 +2,33 @@ import React from 'react'
 
 import Swiper from '../../../components/common/Swiper'
 
+import Nav1 from '../../../assets/images/nav-1.png'
+import Nav2 from '../../../assets/images/nav-2.png'
+import Nav3 from '../../../assets/images/nav-3.png'
+import Nav4 from '../../../assets/images/nav-4.png'
+
 class Index extends React.Component {
-    state = {  }
+    state = { 
+        menu:[
+            {id:1,title:'整租',img:Nav1},
+            {id:2,title:'合租',img:Nav2},
+            {id:3,title:'地图找房',img:Nav3},
+            {id:4,title:'去出租',img:Nav4},
+        ]
+        
+     }
     render() { 
+        const menu  = this.state.menu
+       
         return ( 
             <div>
                 <Swiper />
+                {menu.map((item) => {
+                   return <div key={item.id}>
+                            <img src={item.img} alt=""/>
+                            <span>{item.title}</span>
+                          </div>
+                })}
             </div>
          );
     }
